889eba6ab948e8620292bf06b07396167525ad54
v666 <v666@1nterc3pt0r.org>
2018-04-10 17:24:00 +0200

    Estado inicial de la clase Vehiculo

---
+++ src/m5/uf3/git/project/Vehiculo.java
@@ -0,0 +1,15 @@
+package m5.uf3.git.project;
+
+public class Vehiculo {
+
+	private static final int VELOCIDAD_MAXIMA=120;
+	private int cilindrada;
+	private int puertas;
+	private int velocidadActual;
+	
+	public void acelerarVehiculo() {
+		if(this.velocidadActual!=VELOCIDAD_MAXIMA) {
+			velocidadActual+=10;
+		}
+	}
+}
