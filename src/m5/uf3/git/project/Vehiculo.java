7610ad21a1f10bd93efddfc264ded6f300cfee91
v666 <v666@1nterc3pt0r.org>
2018-04-10 17:57:24 +0200

    Añadidos atributos y métodos nuevos

--- src/m5/uf3/git/project/Vehiculo.java
+++ src/m5/uf3/git/project/Vehiculo.java
@@ -3,15 +3,21 @@ package m5.uf3.git.project;
 public class Vehiculo {
 
 	private static final int VELOCIDAD_MAXIMA=120;
+	private static final int MARCHAS=5;
 	private int cilindrada;
 	private int puertas;
 	private int velocidadActual;
 	
-	public Vehiculo(int cilindrada, int puertas, int velocidadActual) {
+	private int marchas;
+	private int marchaActual;
+	
+	
+	public Vehiculo(int cilindrada, int puertas, int velocidadActual, int marchas, int marchaActual) {
 		super();
 		this.cilindrada = cilindrada;
 		this.puertas = puertas;
 		this.velocidadActual = 0;
+		this.marchaActual=0;
 	}
 
 	public void acelerarVehiculo() {
@@ -24,4 +30,15 @@ public class Vehiculo {
 			velocidadActual-=10;
 		}
 	}
+	public void subirMarcha() {
+		if(this.marchaActual<MARCHAS) {
+			marchaActual++;
+		}
+	}
+	public void bajarMarcha() {
+		if(this.marchaActual<=MARCHAS && this.marchaActual>0) {
+			marchaActual--;
+		}
+	}
+	
 }
